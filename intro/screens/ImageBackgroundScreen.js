4be618debe9191f41a18ebf96068fe8c002a82c2
Fernando <124049519@upq.edu.mx>
2025-10-21 17:06:12 -0600

    Se agrego una imagen de carga en imagebackground y después una para cuando termine de cargar

--- intro/screens/ImageBackgroundScreen.js
+++ intro/screens/ImageBackgroundScreen.js
@@ -1,13 +1,80 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Image, ImageBackground, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
-export default function ImageBackgroundScreen() {
+const BackgroundImage = require("../assets/splash-icon.png");
+const BackgroundImageMain = require("../assets/Aaaaaddw.png");
 
+export default function ImageBackgroundScreen({navigation}) {
+
+  const [showSplash, setShowSplash] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    },3000);
+    return () => clearTimeout(timer);
+  },[]);
+
+  if (showSplash) {
     return (
-      <View>
-        <Text> Proximamente..... </Text>
-      </View>
-    )
+      <ImageBackground source={BackgroundImage} style={styles.background} resizeMode='cover'>
+
+        <View style={styles.overlay}>
+
+          <Text style={styles.title}> Bienvenido </Text>
+          <Text style={styles.title}> Cargando... </Text>
+
+        </View>
+
+      </ImageBackground>
+    );
+  }
+
+  return (
+    <ImageBackground source={BackgroundImageMain} style={styles.background} resizeMode='cover'>
+    <View style={styles.mainScreen}>
+
+      <Text style={styles.mainText2}> Bienvenido a la pantalla principal de la aplicacion </Text>
+      
+    </View>
+    </ImageBackground>
+  );
 
 }
 
-const styles = StyleSheet.create({})
+const {width, height} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  background: {
+    width: width,
+    height: height,
+    justifyContent: 'center',
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  mainScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainText: {
+    fontSize: 24,
+    color: '#000000ff',
+    textAlign: 'center',
+  },
+  mainText2: {
+    fontSize: 18,
+    color: '#ffffffff',
+    textAlign: 'center',
+  },
+});
