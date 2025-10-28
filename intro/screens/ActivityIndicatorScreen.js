7b712c4d0f76590e2a8496823aa46e2e14772730
Fernando <124049519@upq.edu.mx>
2025-10-28 17:26:58 -0600

    Update ActivityIndicatorScreen.js

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,13 +1,46 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Alert, Platform, ActivityIndicator, Button } from 'react-native'
+import React, { useState } from 'react'
 
 export default function ActivityIndicatorScreen() {
 
+  const[cargando, Setcargando] = useState(false);
+  const carga =()=>{ 
+    Setcargando(true);
+    setTimeout(()=>{
+      Setcargando(false);
+      if(Platform.OS === 'web'){
+        window.alert('Carga completa');
+      } else{
+        Alert.alert("Carga completa")
+      }
+    }, 3000);
+  }
+
     return (
-      <View>
-        <Text> Proximamente..... </Text>
+      <View style={styles.container}>
+        <Text style={styles.texto}> Presione para iniciar la carga </Text>
+        <View style={styles.botonesContainer}>
+          {cargando ? (<ActivityIndicator size='large' color='red'> </ActivityIndicator>) : (<Button color='yellow' title='Presiona para iniciar' onPress={carga}> </Button>) }
+        </View>
       </View>
     )
-
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e6e1d5ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    color: '#e2c72dff',
+    fontSize: 30,
+    fontFamily: 'Times New Roman',
+    fontStyle: 'italic'
+  },
+  botonesContainer: {
+    marginTop: 20,
+    gap: 20,
+  },
+})
