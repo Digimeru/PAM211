15cbe2903dcc55fde676467ed03558e0d494949c
Fernando <124049519@upq.edu.mx>
2025-11-10 18:03:15 -0600

    Se agrego la práctica de modal
    A

--- intro/screens/ModalScreen.js
+++ intro/screens/ModalScreen.js
@@ -1,13 +1,58 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Button, Modal } from 'react-native'
+import React, { useState } from "react";
 
 export default function ModalScreen() {
 
+  const [modalVisible, setModalVisible] = useState(false);
+
     return (
-      <View>
-        <Text> Proximamente..... </Text>
+      <View style={styles.container}>
+        <Button title='Mostrar modal' onPress={()=> setModalVisible(true)} color="purple">
+          Puchale
+        </Button>
+
+        <Modal
+        animationType='fade'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={()=>setModalVisible(!modalVisible)}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.textoModal}>
+                Soy un modal omg chatingo
+              </Text>
+              <Button title='Cerrar modal' onPress={()=> setModalVisible(false)} color='red'>
+              </Button>
+            </View>
+          </View>
+        </Modal>
       </View>
     )
 
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 25,
+    borderRadius: 15,
+    alignItems: 'center',
+  },
+  textoModal: {
+    color:'#000',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+})
