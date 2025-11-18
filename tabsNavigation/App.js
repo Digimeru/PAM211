cd5e41d90840d938d373916a769d8ff0442a444a
Fernando <124049519@upq.edu.mx>
2025-11-18 16:40:20 -0600

    Practica No.16 funcionando correctamente

---
+++ tabsNavigation/App.js
@@ -0,0 +1,39 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import Home from './screens/home';
+import Profile from './screens/profile';
+import Settings from './screens/settings';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            let inconName;
+
+            if (route.name === 'Home') {
+              inconName = 'home';
+            } else if (route.name === 'Profile'){
+              inconName = 'person';
+            } else if (route.name === 'Settings'){
+              inconName = 'settings';
+            }
+            return <Ionicons name={inconName} size={size} color={color} />
+          }
+        })}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Profile" component={Profile} />
+          <Tab.Screen name="Settings" component={Settings} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
